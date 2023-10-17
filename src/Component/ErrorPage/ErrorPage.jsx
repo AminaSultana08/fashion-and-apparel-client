cab6883372bd99ed75fd963ff561b96651f12017
AminaSultana08 <aminasultana088@gmail.com>
2023-10-17 22:57:40 +0600

    router added and error 404 page created

---
+++ src/Component/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className=" flex justify-center items-center ">
+            <img src="https://i.ibb.co/Qj2sRc8/404.gif" alt=""/>
+        </div>
+    );
+};
+
+export default ErrorPage;
